678974c8cce29abe0935aee9c3cfa0c55b5b6a70
16BitNarwhal <ericzh1616@gmail.com>
2024-06-30 13:05:53 -0400

    fix and add gtest

--- tests/test_value.cpp
+++ tests/test_value.cpp
@@ -3,6 +3,13 @@
 
 TEST(ValueTest, CreateValue) {
 	Value* a = new Value{5.0};
-	ASSERT_TRUE(a->data == 5.0);
+	ASSERT_EQ(a->data, 5.0);
+}
+
+TEST(ValueTest, Add2Values) {
+	Value a{5.0};
+	Value b{3.0};
+	Value c = a + b;
+	ASSERT_EQ(c.data, 8.0);
 }
 
